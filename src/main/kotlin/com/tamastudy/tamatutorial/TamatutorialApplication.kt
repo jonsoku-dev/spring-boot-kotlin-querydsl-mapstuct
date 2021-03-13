bb51bdd128d25e752e687e6df54eb1a8aa700305
jonsoku2 <whdtjr2792@gmail.com>
2021-03-13 10:15:24 +0900

    init project

---
+++ src/main/kotlin/com/tamastudy/tamatutorial/TamatutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.tamastudy.tamatutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TamatutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<TamatutorialApplication>(*args)
+}
---
+++ src/test/kotlin/com/tamastudy/tamatutorial/TamatutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tamastudy.tamatutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TamatutorialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
